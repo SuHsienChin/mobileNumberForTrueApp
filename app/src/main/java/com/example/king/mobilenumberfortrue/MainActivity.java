28b47c2c3d95169415136a927f00d1872a659874
SuHsienChin <akindpepe@gmail.com>
2018-03-14 16:37:01 +0800

    init

---
+++ app/src/main/java/com/example/king/mobilenumberfortrue/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.king.mobilenumberfortrue;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView mWebView = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mWebView = (WebView)findViewById(R.id.webview);
+
+        //mWebView.setWebViewClient(mWebViewClient);
+        mWebView.loadUrl("http://suhsienchin.000webhostapp.com/index.html");
+
+
+    }
+
+   /* WebViewClient mWebViewClient = new WebViewClient() {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+    };*/
+}
